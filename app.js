a98a596953dcb00b4ea0cc188a0c9d2da0d3ec0d
Shone <shone@journeetrips.com>
2024-07-09 22:51:55 +0530

    Added GET /tasks

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const { default: t } = require('tap');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Array to store tasks
+tasks = [
+    {
+      "id": 1,
+      "title": "Set up environment",
+      "description": "Install Node.js, npm, and git",
+      "completed": true
+    },
+    {
+      "id": 2,
+      "title": "Create a new project",
+      "description": "Create a new project using the Express application generator",
+      "completed": true
+    },
+    {
+      "id": 3,
+      "title": "Install nodemon",
+      "description": "Install nodemon as a development dependency",
+      "completed": true
+    },
+    {
+      "id": 4,
+      "title": "Install Express",
+      "description": "Install Express",
+      "completed": false
+    },
+    {
+      "id": 5,
+      "title": "Install Mongoose",
+      "description": "Install Mongoose",
+      "completed": false
+    },
+  ]
 
 app.listen(port, (err) => {
     if (err) {
@@ -12,6 +47,10 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
 });
 
+
+app.get("/tasks", (req, res) =>{
+    res.send(tasks)
+})
 
 
 module.exports = app;
